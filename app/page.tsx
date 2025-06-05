a8205a470e53564a028115182179d29fb53ae1e6
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-06-05 08:36:18 -0500

    refactor: replace custom VAD hook with useMicVAD and simplify Home component rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,17 @@
+"use client"
+
+import { useMicVAD } from "@ricky0123/vad-react";
 import AudioVisualizer from "./components/visualizer/AudioVisualizer";
-import { useTransition } from "react";
-import useMicVAD from "./lib/useMicVAD";
-import player from "./lib/usePlayer";
-import utils from "./lib/utils";
 
 export default function Home() {
-    const [isPending, startTransition] = useTransition();
-
-    function submit(blob: Blob) {
-        // Implement your submit logic here
-    }
 
     const vad = useMicVAD({
         startOnLoad: true,
-        onSpeechEnd: (audio: Float32Array) => {
-            player.stop();
-            const wav = utils.encodeWAV(audio);
-            const blob = new Blob([wav], { type: "audio/wav" });
-            startTransition(() => submit(blob));
-            const isFirefox = navigator.userAgent.includes("Firefox");
-            if (isFirefox) vad.pause();
-        },
         positiveSpeechThreshold: 0.6,
         minSpeechFrames: 4,
     });
 
     return (
-        <AudioVisualizer vad={vad} />
+        <AudioVisualizer errored={vad.errored} loading={vad.loading} speaking={vad.userSpeaking}/>
     );
 }
