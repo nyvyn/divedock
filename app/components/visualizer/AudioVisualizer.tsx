14b484bbf952906ed5dfe53f9986c6ba60d1ac34
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-06-05 08:38:57 -0500

    refactor: simplify AudioVisualizer props and update opacity logic

--- app/components/visualizer/AudioVisualizer.tsx
+++ app/components/visualizer/AudioVisualizer.tsx
@@ -3,24 +3,21 @@
 import clsx from "clsx";
 import React from "react";
 
-
 interface AudioVisualizerProps {
-    vad: {
-        loading: boolean;
-        errored: boolean;
-        userSpeaking: boolean;
-    };
+    errored: boolean | string;
+    loading: boolean;
+    speaking: boolean;
 }
 
-export default function AudioVisualizer({ vad }: AudioVisualizerProps) {
+export default function AudioVisualizer({ errored, loading, speaking }: AudioVisualizerProps) {
     return (
         <div
             className={clsx(
-                "absolute size-36 blur-3xl rounded-full bg-gradient-to-b from-red-200 to-red-400 dark:from-red-600 dark:to-red-800 -z-50 transition ease-in-out",
+                "size-36 blur-3xl rounded-full bg-gradient-to-b from-red-200 to-red-400 dark:from-red-600 dark:to-red-800 -z-50 transition ease-in-out",
                 {
-                    "opacity-0": vad.loading || vad.errored,
-                    "opacity-30": !vad.loading && !vad.errored && !vad.userSpeaking,
-                    "opacity-100 scale-110": vad.userSpeaking,
+                    "opacity-10": loading || errored,
+                    "opacity-30": !loading && !errored && !speaking,
+                    "opacity-100 scale-110": speaking,
                 }
             )}
         />
