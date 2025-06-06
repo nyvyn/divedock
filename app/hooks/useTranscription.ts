07ccd75007882344e2d5ff46fb52396bfcf8ec5f
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-06-06 12:26:22 -0500

    chore: refactor hooks and clean up unused functionality
    Renamed `useAudioDetection`, `useTranscriber`, and `useTranscription` hooks for consistency. Removed unused VAD logic and decluttered `Home` component. Added stub implementation for `useSynthesis`.

---
+++ app/hooks/useTranscription.ts
@@ -0,0 +1,8 @@
+"use client"
+
+export function useTranscription() {
+
+    return {
+
+    };
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,9 @@
 "use client";
 
-import { useMicVAD } from "@ricky0123/vad-react";
-import { useTransition } from "react";
 import AudioVisualizer from "./components/visualizer/AudioVisualizer";
-import { usePlayer } from "./hooks/usePlayer";
-import { useTranscriber } from "./hooks/useTranscriber";
 
 export default function Home() {
 
-    const [_isPending, startTransition] = useTransition();
-    const player = usePlayer();
-    const {transcribe} = useTranscriber();
-
-    const vad = useMicVAD({
-        model: "v5",
-        ortConfig: (ort) => {
-            ort.env.logLevel = "error";
-        },
-        onSpeechEnd: (audio) => {
-            player.stop();
-            startTransition(() => {
-                transcribe(audio);
-            });
-        },
-        minSpeechFrames: 4,
-        positiveSpeechThreshold: 0.6,
-        startOnLoad: true,
-    });
 
     return (
         <div
