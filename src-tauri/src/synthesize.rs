3e95f8a46fd689d2774f10aaf82d67aa9ff5d7ae
nyvyn <2886602+nyvyn@users.noreply.github.com>
2025-06-07 19:50:29 -0500

    feat: enhance TTS with voice selection and utterance event callbacks emitting app events

--- src-tauri/src/synthesize.rs
+++ src-tauri/src/synthesize.rs
@@ -2,20 +2,43 @@ use anyhow::Result;
 use std::error::Error;
 use std::sync::Mutex;
 use tauri::{AppHandle, Emitter};
-use tts::Tts;
+use tts::{Features, Tts};
 
 pub struct TtsState(Mutex<Tts>);
 
-pub fn init_tts() -> TtsState {
+pub fn init_tts(app: AppHandle) -> TtsState {
     let mut tts = Tts::default().unwrap();       // picks the best backend for the OS
-    let voices = tts.voices().unwrap();          // Vec<tts::Voice>
-    for v in &voices {
-        println!("{} – {}", v.id(), v.name());   // inspect what you have
+
+    let Features { voice, .. } = tts.supported_features();
+    if voice {
+        let voices = tts.voices().unwrap();          // Vec<tts::Voice>
+        let ava = voices.iter().find(|v| v.name() == "Ava").unwrap();
+        tts.set_voice(&ava).unwrap();            // use the chosen voice
     }
 
-    let alex = voices.iter().find(|v| v.name() == "Ralph").unwrap();
-    tts.set_voice(&alex).unwrap();            // use the chosen voice
- 
+    let Features {
+        utterance_callbacks,
+        ..
+    } = tts.supported_features();
+    if utterance_callbacks {
+        let app_clone = app.clone();
+        tts.on_utterance_begin(Some(Box::new(move |utterance| {
+            app_clone.emit("speaking-started", ())
+                .map_err(|e| e.to_string()).unwrap();
+            println!("Started speaking {:?}", utterance)
+        }))).unwrap();
+        tts.on_utterance_end(Some(Box::new(move |utterance| {
+            app_clone.emit("speaking-stopped", ())
+                .map_err(|e| e.to_string()).unwrap();
+            println!("Finished speaking {:?}", utterance)
+        }))).unwrap();
+        tts.on_utterance_stop(Some(Box::new(move |utterance| {
+            app_clone.emit("speaking-stopped", ())
+                .map_err(|e| e.to_string()).unwrap();
+            println!("Stopped speaking {:?}", utterance)
+        }))).unwrap();
+    }
+
     TtsState(Mutex::new(tts))
 }
 
